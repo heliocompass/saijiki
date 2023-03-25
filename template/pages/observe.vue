6fae96a75336b1c5a37d2fb9765b6741f8b29bd4
IMAMURA Shohei <s3imamura@gmail.com>
2023-03-25 17:08:14 +0900

    [template] add date picker

--- template/pages/observe.vue
+++ template/pages/observe.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import species from '~~/data/species';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref(new Date())
+const format = (date: Date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${year}/${month}/${day}`;
+}
 </script>
 
 
@@ -14,6 +25,9 @@ import species from '~~/data/species';
                 capture="camera"></v-file-input>
             <v-autocomplete variant="outlined" density="compact" label="観測種目" item-value="id" item-title="species"
                 :items="species"></v-autocomplete>
+            <VueDatePicker :format="format" :enable-time-picker="false" disable-month-year-select :max-date="new Date()"
+                :clearable="false" text-input auto-apply locale="jp" v-model="date" class="mb-4">
+            </VueDatePicker>
             <v-text-field label="タイトル"></v-text-field>
             <v-textarea label="メモ" variant="outlined"></v-textarea>
             <v-btn type="submit" color="primary" block class="mt-2">投稿</v-btn>
