8b7d81088f49373f00b964723e15aac10d16de2f
IMAMURA Shohei <s3imamura@gmail.com>
2023-03-25 19:48:32 +0900

    [template] add google map

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,23 +1,67 @@
+<script setup lang="ts">
+import { Loader } from '@googlemaps/js-api-loader'
+
+const currentPosition = ref({ lat: 35.681236, lng: 139.767125 });
+
+onMounted(async () => {
+    getCurrentPosition();
+    const loader = new Loader({
+        apiKey: 'AIzaSyBWHhQO8dhd06rN8lZUckxaPv_VY9ZY30o',
+        version: 'weekly',
+    })
+
+    const google = await loader.load()
+    const map = new google.maps.Map(document.getElementById('google-map'), {
+        center: currentPosition.value,
+        zoom: 15,
+        disableDefaultUI: true,
+        styles: [
+            {
+                featureType: 'poi',
+                stylers: [{ visibility: 'off' }],
+            },
+            {
+                featureType: 'transit',
+                stylers: [{ visibility: 'off' }],
+            },
+        ],
+    })
+},)
+
+const getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(`現在位置: (${latitude}, ${longitude})`);
+            currentPosition.value = { lat: latitude, lng: longitude };
+
+        },
+        (error) => {
+            console.error(`現在位置の取得に失敗しました: ${error.message}`);
+        }
+    );
+}
+
+</script>
 <template>
-    <v-app-bar color="grey-darken-2" density="compact">
-        <v-app-bar-title>Map</v-app-bar-title>
-    </v-app-bar>
-    <v-card id="google-map">
-        <v-btn icon rounded elevation="2" id="add-btn" to="/observe">
-            <v-icon size="x-large">mdi-camera</v-icon>
-        </v-btn>
-    </v-card>
+    <v-container class="ma-0 pa-0">
+
+        <v-app-bar color="grey-darken-2" density="compact">
+            <v-app-bar-title>Map</v-app-bar-title>
+        </v-app-bar>
+
+        <div id="google-map" flat>
+
+        </div>
+    </v-container>
 </template>
 
 <style scoped>
 #google-map {
-    height: 100vh;
+    position: abasolute;
+    /* ヘッダーとボタンナビゲーションの高さをマイナス */
+    height: calc(100vh - 48px - 56px);
     width: 100vw;
-}
-
-#add-btn {
-    position: absolute;
-    bottom: 8rem;
-    right: 2rem;
 }
 </style>
